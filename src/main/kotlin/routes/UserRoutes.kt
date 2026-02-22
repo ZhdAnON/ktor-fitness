2db7a653126d4dc3538d87c00c29b04bc700cee7
Zhdanov Andrey <zhdanon85@yandex.ru>
2026-02-22 18:58:34 +0500

    added route /user/password

--- src/main/kotlin/routes/UserRoutes.kt
+++ src/main/kotlin/routes/UserRoutes.kt
@@ -1,11 +1,14 @@
 package com.zhdanon.routes
 
+import com.zhdanon.models.response.UpdatePasswordRequestDto
 import com.zhdanon.models.response.UserResponse
 import com.zhdanon.repository.UserRepository
+import com.zhdanon.utils.hashPassword
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import java.util.*
@@ -13,6 +16,7 @@ import java.util.*
 fun Application.userRoutes(userRepository: UserRepository) {
     routing {
         authenticate("auth-jwt") {
+
             get("/user/profile") {
                 val principal = call.principal<JWTPrincipal>()!!
                 val userId = principal.payload.getClaim("userId").asString()
@@ -28,6 +32,18 @@ fun Application.userRoutes(userRepository: UserRepository) {
                         createdAt = user.createdAt.toString()
                     )
                 )
+            }
+
+            put("/user/password") {
+                val principal = call.principal<JWTPrincipal>()!!
+                val userId = principal.payload.getClaim("userId").asString()
+
+                val req = call.receive<UpdatePasswordRequestDto>()
+
+                val hash = hashPassword(req.password)
+                userRepository.updatePassword(UUID.fromString(userId), hash)
+
+                call.respond(HttpStatusCode.OK)
             }
         }
     }
